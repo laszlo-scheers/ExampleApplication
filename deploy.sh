6f1041e7b094c9cfbf21d56da464b2cb0480213b
Laszlo-VSC <laszloluque@gmail.com>
2022-03-30 18:02:29 +0100

    added a command to stop the pm2 CA server

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,8 @@ if [ "$CURRENT_INSTANCE" ]
 then
     docker rm $(docker stop $CURRENT_INSTANCE)
 fi
-
+# If CA app is running will stop it
+pm2 stop simple_app
 # Pull down the instance from dockerhub
 docker pull $IMAGE_NAME
 
@@ -15,11 +16,11 @@ docker pull $IMAGE_NAME
 CONTAINER_EXISTS=$(docker ps -a | grep $CONTAINER_NAME)
 if [ "$CONTAINER_EXISTS" ]
 then
-    docker rm node_app
+    docker rm $CONTAINER_NAME
 fi
 
 # Create a container called node_app that is available on port 8443 from our docker image
-docker create -p 8443:8443 --name node_app $IMAGE_NAME
+docker create -p 8443:8443 --name $CONTAINER_NAME $IMAGE_NAME
 # Write the private key to a file
 echo $PRIVATE_KEY > privatekey.pem
 # Write the server key to a file
@@ -29,4 +30,4 @@ docker cp ./privatekey.pem node_app:/privatekey.pem
 # Add the server key to the node_app docker container
 docker co ./server.crt node_app:/server.crt
 # Starts the node_app container
-docker start node_app
+docker start $CONTAINER_NAME
